0cbe0e61b7b3c3efa0f78076a4ed5d0ac4b6f3f3
WelcomeParkhyejin <zeze2787@daum.net>
2022-03-06 18:12:09 +0900

    skill section

--- parkhye/scripts/main.js
+++ parkhye/scripts/main.js
@@ -10,3 +10,20 @@ function MobileNavClose(mobileClose) {
 function GotoTop() {
     $( 'html, body' ).animate( { scrollTop : 0 }, 400 );
 };
+//섹션이동
+$('.header-nav ul li a').on('click', function() {  
+    $('html, body').animate({scrollTop: $(this.hash).offset().top - 50}, 1000);
+    return false;
+});
+//스크롤 시 헤더 active 생성
+let header = $('.header');
+
+    $(window).scroll(function(e){
+        if(header.offset().top !== 0){
+            if(!header.hasClass('active')){
+                header.addClass('active');
+            }
+        }else{
+            header.removeClass('active');
+        }
+    });
